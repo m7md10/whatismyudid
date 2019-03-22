c352b47af7032fc41ec2579e91d8ddc8d476db39
m7md10 <m.alokaily@gmail.com>
2019-03-22 14:08:21 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@
 exports.index = function(req, res){
 
     var ua = require('useragent');
-	var domain = 'udid.fyi';
+	var domain = 'udid882992929292.herokuapp.com';
     if (!ua.is(req.headers['user-agent']).mobile_safari)
     {
         res.render('index-notios', { title: domain});
